0802ae1c46c3fa67de9754cb75d604191a21c4fe
yena <dbdpsk8871@naver.com>
2023-09-07 11:10:34 +0900

    HistoryList update

---
+++ vue3/vue3portfolio/src/components/HistoryItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="history-item">
+    <button type="button" class="history-item__slide"  @click="$emit('handleClick')">
+      <div class="history-item__img">
+        <img :src="`images/${historyImg}.png`" :alt="historyImg">
+      </div>
+      <span v-if="imgDesc.length > 0" class="history-item__imgdesc"> {{ imgDesc }}</span>
+    </button>
+    <span class="history-item__project-tag">{{ tag }}</span>
+    <span class="history-item__project-tit"> {{ tit }}</span> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'historyItem',
+  props: {
+    historyImg: {
+      type: String,
+      default: '',
+    },
+    imgDesc: {
+      type: String,
+      default: '',
+    },
+    tag: {
+      type: String,
+      default: '',
+    },
+    tit: {
+      type: String,
+      default: '',
+    },
+  },
+  emits: ['handleClick']  
+}
+
+</script>
+
+<style lang="scss">
+  .history-item {
+    $this: '.history-item';
+    &__slide {
+      text-align: left;
+    }
+    &__imgdesc {
+      color: #666;
+      font-size: 1.5rem;
+    }
+    &__project-tag {
+      display: inline-flex;
+      background-color: #c8cffa;
+      color: #3042e6;
+      font-size: 2.8rem;
+      border-radius: 1.5rem;
+      padding: 1rem 1.5rem;
+      margin: 7rem 0 2rem 2rem;
+    }
+    &__project-tit {
+      display: block;
+      font-size: 4rem;
+      padding: 0 2rem 0;
+    }
+  }
+</style>
--- vue3/vue3portfolio/src/views/HistoryList.vue
+++ vue3/vue3portfolio/src/views/HistoryList.vue
@@ -11,7 +11,16 @@
         clickable: true,
       }"
     >
-      <swiper-slide>
+    <swiper-slide v-for="(item, index) in swiperItem" :key="index">
+      <historyItem
+        :historyImg = "item.historyImg"
+        :imgDesc = "item.imgDesc"
+        :tag = "item.tag"
+        :tit ="item.tit"
+        @handleClick="handleClick(index)"
+      />
+    </swiper-slide>
+      <!-- <swiper-slide>
         <button type="button" class="history-list__slide" @click="isHecto">
           <div class="history-list__img">
             <img src="../assets/images/img_hecto.png" alt="헥토헬스케어 이미지">
@@ -56,33 +65,36 @@
         </div>
         <span class="history-list__project-tag">웹 사이트</span>
         <span class="history-list__project-tit">한국국제류재단 운영</span> 
-      </swiper-slide>
+      </swiper-slide> -->
     </swiper>
     <div class="history-list__swiper-pagination"></div>
 
     <!-- hecto 세부내용 -->
-    <div v-if="openHecto" class="history-list__detail">
-      <span class="history-list__project-date">2023.05 ~ 2023.08</span>
+    <div v-show="activeIndex === list" v-for="(box, list) in descItem" :key="list" class="history-list__detail">
+      <span class="history-list__project-date">{{ box.date }}</span>
+      <span class="history-list__project-personnel">{{ box.personnel }}</span>
+      <span class="history-list__project-desc" v-html="box.desc"></span>
+    </div>
+    <!-- <span class="history-list__project-date">2023.05 ~ 2023.08</span>
       <span class="history-list__project-personnel">개발 인원 : 프론트 2명, 백 6명(고객사)</span>
       <span class="history-list__project-desc">        
         양방향 바인딩이 가능한 뷰의 장점을 살려 자주 사용되는 요소(제품 슬라이드, 배송 현황 바, 그래프 등)들은 <strong>컴포넌트로 제작</strong>하여 코드 재사용률을 높혔다. <br />총 130 페이지 중 75% 개발
-      </span>
-    </div>
-
+      </span> -->
     <!-- lg 세부내용 -->
-    <div v-if="openLG" class="history-list__detail">
+    <!-- <div v-if="openLG" class="history-list__detail">
       <span class="history-list__project-date">2023.04</span>
       <span class="history-list__project-personnel">개발 인원 : 프론트 5명</span>
       <span class="history-list__project-desc">        
         언어별 디자인에 따라 코드 개발 하였으며, 9개국 중 호주와 멕시코를 담당, UK의 카트, checkout, search, Quick menu, About LG 페이지 수정을 담당<br />
         <br />PUG를 사용하며 <strong>mixin을 통해 코드 간소화</strong>를 진행하였다
       </span>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import historyItem from '@/components/HistoryItem.vue'
 import { Pagination } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
@@ -90,14 +102,45 @@ import 'swiper/css/pagination'
 
 const modules = [Pagination]
 
-const openHecto = ref(false)
-const isHecto = () => {
-  openHecto.value = !openHecto.value
-}
-const openLG = ref(false)
-const isLg = () => {
-  openLG.value = !openLG.value
-}
+const activeIndex = ref(0);
+const handleClick = (index) => {
+  activeIndex.value = index;
+} 
+
+const swiperItem = ([
+  {
+    historyImg: 'img_hecto',
+    imgDesc: '헥토헬스케어 홈페이지 이미지입니다. (관련 어플은 10월 초 오픈예정)',
+    tag: '모바일',
+    tit: '헥토헬스케어 구축',
+  },
+  {
+    historyImg: 'img_lg',
+    tag: '웹사이트(반응형)',
+    tit: 'LG.com GP1 2차 구축',
+  },
+])
+const descItem = ref ([
+  {
+    date: '2023.05 ~ 2023.08',
+    personnel: '개발 인원 : 프론트 2명, 백 6명(고객사)',
+    desc: '양방향 바인딩이 가능한 뷰의 장점을 살려 자주 사용되는 요소(제품 슬라이드, 배송 현황 바, 그래프 등)들은 <strong>컴포넌트로 제작</strong>하여 코드 재사용률을 높혔다. <br />총 130 페이지 중 75% 개발',
+  },
+  {
+    date: '2023.04',
+    personnel: '개발 인원 : 프론트 5명',
+    desc: '언어별 디자인에 따라 코드 개발 하였으며, 9개국 중 호주와 멕시코를 담당, UK의 카트, checkout, search, Quick menu, About LG 페이지 수정을 담당 <br />PUG를 사용하며 <strong>mixin을 통해 코드 간소화</strong>를 진행하였다',
+  },
+])
+
+// const openHecto = ref(false)
+// const isHecto = () => {
+//   openHecto.value = !openHecto.value
+// }
+// const openLG = ref(false)
+// const isLg = () => {
+//   openLG.value = !openLG.value
+// }
 
 
 </script>
@@ -116,13 +159,6 @@ const isLg = () => {
     margin: 7rem 0 3rem;
     font-size: 2.5rem;
   }
-  &__slide {
-    text-align: left;
-  }
-  &__imgdesc {
-    color: #666;
-    font-size: 1.5rem;
-  }
   &__swiper-pagination {
     position: absolute;
     bottom: 0;
@@ -139,20 +175,7 @@ const isLg = () => {
       background-color: #000;
     }
   }
-  &__project-tag {
-    display: inline-flex;
-    background-color: #c8cffa;
-    color: #3042e6;
-    font-size: 2.8rem;
-    border-radius: 1.5rem;
-    padding: 1rem 1.5rem;
-    margin: 7rem 0 2rem 2rem;
-  }
-  &__project-tit {
-    display: block;
-    font-size: 4rem;
-    padding: 0 2rem 0;
-  }
+  
   &__detail {
     padding-left: 2rem;
     margin-top: 1rem;

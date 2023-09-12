0b6db2f528c3775ff212553abc60cd884f672184
yena <dbdpsk8871@naver.com>
2023-09-12 11:28:48 +0900

    phoneMain page bottom app update

--- vue3/vue3portfolio/src/views/PhoneMain.vue
+++ vue3/vue3portfolio/src/views/PhoneMain.vue
@@ -11,6 +11,17 @@
           <span class="phone-main__icontxt">캘린더</span>
         </div>
       </div>
+      <div class="phone-main__bottombox">
+        <div class="phone-main__app">
+          <button class="phone-main__icon phone-main__icon--call"><span class="hide">전화걸기</span></button>
+          <button class="phone-main__icon phone-main__icon--safari"><span class="hide">인터넷 연결하기</span></button>
+          <button class="phone-main__icon phone-main__icon--send">
+            <div class="phone-main__message"></div>
+            <span class="hide">메세지보내기</span>
+          </button>
+          <button class="phone-main__icon phone-main__icon--music"><span class="hide">음악듣기</span></button>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -73,8 +84,13 @@
   }
 }
 .phone-main {
+  $this: '.phone-main';
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
   &__appbox {
-    padding: 4rem 3rem;
+    padding: 5rem 3rem;
   }
   &__app {
     position: relative;
@@ -82,18 +98,55 @@
     text-align: center;
   }
   &__icon {
-    width: 11rem;
-    height: 11rem;
-    border-radius: 1.5rem;
+    width: 12rem;
+    height: 12rem;
+    border-radius: 2rem;
+    padding: 1rem;
+    text-align: center;
+    box-shadow: 1px 1px 2rem rgb(0, 0, 0, 0.4);
     &--calender {
       background-color: #fff;
+    }
+    &--call {
+      background: #35c235 url(../assets/images/icon_call.svg) center / 70% no-repeat;
+    }
+    &--safari {
+      background: #fff url(../assets/images/icon_safari.png) center / contain no-repeat;
+    }
+    &--send {
+      background-color: #35c235;
+    }
+    &--music {
+      background: #fb4f67 url(../assets/images/icon_music.svg) center / 70% no-repeat;
+    }
+  }
+  &__message {
+    position: relative;
+    width: 9rem;
+    height: 7.5rem;
+    background-color: #fff;
+    border-radius: 50%;
+    margin: 0 auto 1rem;
+    z-index: 999;
+    &::after {
+      content: '';
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 1.8rem;
+      bottom: -1.7rem;
+      border-left: 1.2rem solid transparent;
+      border-right: 1.2rem solid transparent;
+      border-top: 3.4rem solid #fff;
+      transform: rotate(30deg);
     }
   }
   &__day {
     display: block;
     color: #000;
-    font-size: 1.5rem;
+    font-size: 2.3rem;
     font-weight: 500;
+    margin-bottom: 0.3rem;
   }
   &__date {
     display: block;
@@ -107,6 +160,17 @@
     color: #fff;
     font-size: 2.5rem;
     margin-top: 2rem;
+  }
+  &__bottombox {
+    padding: 5rem;
+    border-radius: 8rem;
+    background-color: rgba(0, 0, 0, 0.5);
+    margin-bottom: 5rem;
+    #{$this}__app {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
   }
 }
 </style>
